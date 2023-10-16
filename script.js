0dec88c5ecd42c9bdf7e0c25daf756c0ab3be545
CodingChimpenze <147919538+CodingChimpenze@users.noreply.github.com>
2023-10-16 11:44:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// This script is used for the typewriter effect. You can place it in a separate "script.js" file.
+// script.js
 
 document.addEventListener("DOMContentLoaded", () => {
   const businessName = document.getElementById("businessName");
   businessName.textContent = ""; // Clear the text initially.
 
-  const text = "Your Business Name";
+  const text = "Coding Chimpenze";
   let charIndex = 0;
 
   function typeText() {
