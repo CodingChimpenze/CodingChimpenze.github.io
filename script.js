765d13bb725fda7b88b7833154053aada34cac85
CodingChimpenze <147919538+CodingChimpenze@users.noreply.github.com>
2023-10-16 12:44:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,1 @@
-// script.js
 
-document.addEventListener("DOMContentLoaded", () => {
-  const businessName = document.getElementById("businessName");
-  businessName.textContent = ""; // Clear the text initially.
-
-  const text = "Coding Chimpenze";
-  let charIndex = 0;
-
-  function typeText() {
-    if (charIndex < text.length) {
-      businessName.textContent += text.charAt(charIndex);
-      charIndex++;
-      setTimeout(typeText, 150);
-    }
-  }
-
-  typeText();
-});
